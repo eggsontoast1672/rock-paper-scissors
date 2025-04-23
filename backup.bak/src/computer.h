167c6b86dc37f3d8ca991e0573a5458cd35895c3
Paul <paul.zupan.13@gmail.com>
2025-04-22 22:32:39 -0700

    Remove backup folder

--- backup.bak/src/computer.h
+++
@@ -1,8 +0,0 @@
-#ifndef RPS_COMPUTER_H
-#define RPS_COMPUTER_H
-
-#include "move.h"
-
-RpsMove rps_computer_get_move(void);
-
-#endif
--- backup.bak/src/player.h
+++
@@ -1,8 +0,0 @@
-#ifndef RPS_PLAYER_H
-#define RPS_PLAYER_H
-
-#include "move.h"
-
-RpsMove rps_player_get_move(void);
-
-#endif
